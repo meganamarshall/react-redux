2205fca55ab475f5f1dd7860eb0f858921eb9aef
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-05-22 16:09:28 -0700

    update post test written/passing

--- src/actions/postActions.test.js
+++ src/actions/postActions.test.js
@@ -1,4 +1,4 @@
-import { ADD_POST, addPost, deletePost, DELETE_POST } from './postActions';
+import { ADD_POST, addPost, deletePost, DELETE_POST, updatePost, UPDATE_POST } from './postActions';
 
 describe('post actions', () =>{
   it('creates and add post action', () => {
@@ -15,6 +15,16 @@ describe('post actions', () =>{
     expect(deletePost('12345')).toEqual({
       type: DELETE_POST,
       payload: '12345'
+    });
+  });
+  it('creates an update post action', () => {
+    const post = {
+      postBody: 'hello',
+      postId: '12345'
+    };
+    expect(updatePost(post)).toEqual({
+      type: UPDATE_POST,
+      payload: post
     });
   });
 });
