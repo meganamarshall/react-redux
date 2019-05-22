c6487dd0ab6d80ab82737aa3a6969b52ec3b5116
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-05-22 15:57:16 -0700

    post actions

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -1,3 +1,21 @@
 const ADD_POST = 'ADD_POST';
 const DELETE_POST = 'DELETE_POST';
+const UPDATE_POST = 'UPDATE_POST';
 
+const addPost = post => ({
+  type: ADD_POST,
+  payload: post
+});
+
+const deletePost = postId => ({
+  type: DELETE_POST,
+  payload: postId
+});
+
+const updatePost = (postId, postBody) => ({
+  type: UPDATE_POST,
+  payload: postId, postBody
+});
+
+
+export { ADD_POST, DELETE_POST, addPost, deletePost, updatePost, UPDATE_POST };
