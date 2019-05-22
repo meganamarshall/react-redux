2e96a7059842f4f8fe2962321d2f2fab970b7845
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-05-22 14:32:42 -0700

    working on reducer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from './components/App';
 
-render(
-  <App />,
-  document.getElementById('root')
-);
+import { createStore } from 'redux';
+
+const initialState = {
+  drink: null,
+  sandwich: null,
+  chips: null
+};
+
+function reducer(state, action) {
+  switch(action.type) {
+    case 'ADD_DRINK':
+      return { ...state, drink: action.payload };
+    case 'ADD_SANDWICH':
+      return { ...state, sandwich: action.payload };
+    case 'ADD_CHIPS':
+      return { ...state, chips: action.payload };
+    case 'REMOVE_DRINK':
+      return { ...state, drink: null };
+    case 'REMOVE_SANDWICH':
+      return { ...state, sandwich: null };
+    case 'REMOVE_CHIPS':
+      return { ...state, chips: null };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+
+
