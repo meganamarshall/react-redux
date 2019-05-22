031e337ea611a7cab8aaf1a5d3c9a524187f3f0f
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-05-22 15:44:23 -0700

    refactor complete

--- src/actions/lunchActions.js
+++ src/actions/lunchActions.js
@@ -11,8 +11,32 @@ const addDrink = drink => ({
   payload: drink
 });
 
+const addSandwich = sandwich => ({
+  type: ADD_SANDWICH,
+  payload: sandwich
+});
+
+const addChips = chips => ({
+  type: ADD_CHIPS,
+  payload: chips
+});
+
+const removeDrink = () => ({
+  type: REMOVE_DRINK
+});
+
+const removeChips = () => ({
+  type: REMOVE_CHIPS
+});
+
+const removeSandwich = () => ({
+  type: REMOVE_SANDWICH
+});
+
+const removeOneChip = chip => ({
+  type: REMOVE_ONE_CHIP,
+  payload: chip
+});
 
 
-
-
-export { ADD_DRINK, ADD_SANDWICH, ADD_CHIPS, REMOVE_CHIPS, REMOVE_DRINK, REMOVE_SANDWICH, REMOVE_ONE_CHIP, addDrink };
+export { ADD_DRINK, ADD_SANDWICH, ADD_CHIPS, REMOVE_CHIPS, REMOVE_DRINK, REMOVE_SANDWICH, REMOVE_ONE_CHIP, addDrink, addSandwich, addChips, removeDrink, removeChips, removeSandwich, removeOneChip };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 
 import { createStore } from 'redux';
-import { ADD_DRINK, addDrink } from './actions/lunchActions';
+import { ADD_DRINK, addDrink, ADD_SANDWICH, ADD_CHIPS, REMOVE_DRINK, REMOVE_SANDWICH, REMOVE_CHIPS, REMOVE_ONE_CHIP, addSandwich, addChips, removeDrink, removeChips, removeSandwich, removeOneChip } from './actions/lunchActions';
 
 
 const initialState = {
@@ -13,17 +13,17 @@ function reducer(state = initialState, action) {
   switch(action.type) {
     case ADD_DRINK:
       return { ...state, drink: [...state.drink, action.payload] };
-    case 'ADD_SANDWICH':
+    case ADD_SANDWICH:
       return { ...state, sandwich: [...state.sandwich, action.payload] };
-    case 'ADD_CHIPS':
+    case ADD_CHIPS:
       return { ...state, chips: [...state.chips, action.payload] };
-    case 'REMOVE_DRINK':
+    case REMOVE_DRINK:
       return { ...state, drink: [] };
-    case 'REMOVE_SANDWICH':
+    case REMOVE_SANDWICH:
       return { ...state, sandwich: [] };
-    case 'REMOVE_CHIPS':
+    case REMOVE_CHIPS:
       return { ...state, chips: [] };
-    case 'REMOVE_ONE_CHIP':
+    case REMOVE_ONE_CHIP:
       return { ...state, chips: state.chips.filter(item => item !== action.payload) };
     default:
       return state;
@@ -38,47 +38,29 @@ store.dispatch(addDrink('Regular Coke'));
 
 console.log('drink added', store.getState());
 
-store.dispatch({
-  type: 'ADD_SANDWICH',
-  payload: 'Field Roast'
-});
+store.dispatch(addSandwich('Field Roast'));
 
 console.log('sandwich added', store.getState());
 
-store.dispatch({
-  type: 'ADD_CHIPS',
-  payload: 'Juanitas'
-});
+store.dispatch(addChips('Juanitas'));
 
-store.dispatch({
-  type: 'ADD_CHIPS',
-  payload: 'Doritos'
-});
+store.dispatch(addChips('Doritos'));
 
 console.log('chips added', store.getState());
 
-store.dispatch({
-  type: 'REMOVE_ONE_CHIP',
-  payload: 'Doritos'
-});
+store.dispatch(removeOneChip('Doritos'));
 
 console.log('one chip removed', store.getState());
 
-store.dispatch({
-  type: 'REMOVE_DRINK'
-});
+store.dispatch(removeDrink());
 
 console.log('drink removed', store.getState());
 
-store.dispatch({
-  type: 'REMOVE_SANDWICH'
-});
+store.dispatch(removeSandwich());
 
 console.log('sandwich removed', store.getState());
 
-store.dispatch({
-  type: 'REMOVE_CHIPS'
-});
+store.dispatch(removeChips());
 
 console.log('chips removed', store.getState());
 
