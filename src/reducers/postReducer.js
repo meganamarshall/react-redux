3b1da7e9dca7d052bc8cb6a628f8448b7aced58e
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-05-22 16:19:00 -0700

    added post reducer

---
+++ src/reducers/postReducer.js
@@ -0,0 +1,7 @@
+import { addPost, ADD_POST, deletePost, DELETE_POST, updatePost, UPDATE_POST } from '../actions/postActions';
+
+initialState = {
+  posts: []
+}
+function reducer(state = initialState, action)
+ 
