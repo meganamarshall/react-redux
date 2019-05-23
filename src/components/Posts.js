d589ec1c0fcde303074288b803ec1c66c735caf4
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-05-23 14:39:01 -0700

    Posts component

---
+++ src/components/Posts.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Post from './Post';
+
+function Posts({ posts, deletePost }) {
+  const postList = posts.map(post => (
+    <li key={post.postId}>
+      <Post deletePost={deletePost} post={post} />
+    </li>
+  ));
+
+  return (
+    <ul>
+      {postList}
+    </ul>
+  );
+}
+
+Posts.propTypes = {
+  deletePost: PropTypes.func.isRequired,
+  posts: PropTypes.shape({
+    postId: PropTypes.number.isRequired,
+    postTitle: PropTypes.string.isRequired
+  })
+};
+
+export default Posts;
+
