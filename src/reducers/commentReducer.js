2a8257a90a5320ab5174d3d4ef0c4e4b8b79a583
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-05-23 10:38:11 -0700

    add comment works if its the first comment

--- src/reducers/commentReducer.js
+++ src/reducers/commentReducer.js
@@ -3,6 +3,6 @@ import { ADD_COMMENT } from '../actions/commentActions';
 export default function reducer(state = {}, action) {
   switch(action.type) {
     case ADD_COMMENT:
-      return { ...state, [action.payload.postId]: [...state[action.payload.postId], action.payload.comment] };
+      return { ...state, [action.payload.postId]: [...(state[action.payload.postId] || []), action.payload.comment] };
   }
 }
--- src/reducers/commentReducer.test.js
+++ src/reducers/commentReducer.test.js
@@ -8,4 +8,10 @@ describe('comment reducer', () => {
       0: ['hi', 'hello there']
     });
   });
+  it('handles the add comment when its the first comment', () => {
+    const newState = reducer({}, addComment(0, 'why'));
+    expect(newState).toEqual({
+      0: ['why']
+    });
+  });
 });
