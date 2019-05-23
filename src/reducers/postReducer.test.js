fedb54cd04b52bdb08c38435ab6f9125413dc1f2
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-05-22 17:22:50 -0700

    added post title/wrote update post test

--- src/reducers/postReducer.test.js
+++ src/reducers/postReducer.test.js
@@ -1,23 +1,32 @@
 import reducer from './postReducer';
-import { addPost, deletePost } from '../actions/postActions';
+import { addPost, deletePost, updatePost } from '../actions/postActions';
 
 describe('post reducer', () => {
   it('handles the add post action', () => {
     const initialState = {
       posts: []
     };
-    const newState = reducer(initialState, addPost({ postId: '1234', postBody: 'hello' }));
+    const newState = reducer(initialState, addPost({ postId: '1234', postBody: 'hello', postTitle: 'hello2' }));
     expect(newState).toEqual({
-      posts: [{ postId: '1234', postBody: 'hello' }]
+      posts: [{ postId: '1234', postBody: 'hello', postTitle: 'hello2' }]
     });
   });
   it('handles the delete post action', () => {
     const initialState = {
-      posts: [{ postId: '1234', postBody: 'hello' }, { postId: '5678', postBody: 'hello there' }]
+      posts: [{ postId: '1234', postBody: 'hello', postTitle: 'title' }, { postId: '5678', postBody: 'hello there', postTitle: 'title' }]
     };
     const newState = reducer(initialState, deletePost('1234'));
     expect(newState).toEqual({
-      posts: [{ postId: '5678', postBody: 'hello there' }]
+      posts: [{ postId: '5678', postBody: 'hello there', postTitle: 'title' }]
+    });
+  });
+  it('handles the update post action', () => {
+    const initialState = {
+      posts: [{ postId: '1234', postBody: 'hello', postTitle: 'title' }, { postId: '5678', postBody: 'hello there', postTitle: 'title' }]
+    };
+    const newState = reducer(initialState, updatePost('1234', 'new bod'));
+    expect(newState).toEqual({
+      posts: [{ postId: '1234', postBody: 'new bod', postTitle: 'title' }, { postId: '5678', postBody: 'hello there', postTitle: 'title' }]
     });
   });
 });
