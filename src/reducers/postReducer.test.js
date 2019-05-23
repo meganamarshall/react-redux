aeb6b5c8871c6ddb750b6bd04f29fe0302496c72
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-05-22 17:08:32 -0700

    delete post test written

--- src/reducers/postReducer.test.js
+++ src/reducers/postReducer.test.js
@@ -1,5 +1,5 @@
 import reducer from './postReducer';
-import { addPost } from '../actions/postActions';
+import { addPost, deletePost } from '../actions/postActions';
 
 describe('post reducer', () => {
   it('handles the add post action', () => {
@@ -9,6 +9,15 @@ describe('post reducer', () => {
     const newState = reducer(initialState, addPost({ postId: '1234', postBody: 'hello' }));
     expect(newState).toEqual({
       posts: [{ postId: '1234', postBody: 'hello' }]
+    });
+  });
+  it('handles the delete post action', () => {
+    const initialState = {
+      posts: [{ postId: '1234', postBody: 'hello' }, { postId: '5678', postBody: 'hello there' }]
+    };
+    const newState = reducer(initialState, deletePost('1234'));
+    expect(newState).toEqual({
+      posts: [{ postId: '5678', postBody: 'hello there' }]
     });
   });
 });
