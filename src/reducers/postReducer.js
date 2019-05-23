c69a5d6e18c245ca8570e05539a0b44291cd470d
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-05-22 17:02:23 -0700

    working on postReducer

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -1,7 +1,14 @@
 import { addPost, ADD_POST, deletePost, DELETE_POST, updatePost, UPDATE_POST } from '../actions/postActions';
 
-initialState = {
+const initialState = {
   posts: []
 }
-function reducer(state = initialState, action)
+export default function reducer(state = initialState, action) {
+  switch(action.type) {
+    case ADD_POST:
+      return { ...state, posts: [...state.posts, action.payload] };
+    case DELETE_POST:
+      return { ...state, }
+  }
+}
  
