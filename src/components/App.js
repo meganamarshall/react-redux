d7b79efc600a1cb7ae34b311a74f89da353c3a32
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-05-23 16:00:13 -0700

    working on app

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import AllPosts from '../containers/AllPosts';
+import CreatePost from '../containers/CreatePost';
+
+export default function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/allposts" component={AllPosts} />
+        <Route path="/CreatePost" component={CreatePost} />
+      </Switch>
+    </Router>
+  );
+}
