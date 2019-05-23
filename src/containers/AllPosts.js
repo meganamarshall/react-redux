0c33745b4a7ca0dc5ed1dab97aad4455818cf3b8
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-05-23 15:43:54 -0700

    allposts container

---
+++ src/containers/AllPosts.js
@@ -0,0 +1,20 @@
+import { connect } from 'react-redux';
+import { getPosts } from '../selectors/postSelectors';
+import Posts from '../components/Posts';
+import { deletePost } from '../actions/postActions';
+
+const mapStateToProps = state => ({
+  posts: getPosts(state)
+});
+
+const mapDispatchToProps = dispatch => ({
+  deletePost(postId) {
+    dispatch(deletePost(postId)); 
+  }
+});
+
+export default connect(
+  null,
+  mapStateToProps,
+  mapDispatchToProps
+)(Posts);
