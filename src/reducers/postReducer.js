b2a91ca2fae6d337e47f16a20362e3d6f7f6a928
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-05-22 17:17:52 -0700

    delete post written

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -2,13 +2,14 @@ import { addPost, ADD_POST, deletePost, DELETE_POST, updatePost, UPDATE_POST } f
 
 const initialState = {
   posts: []
-}
+};
+
 export default function reducer(state = initialState, action) {
   switch(action.type) {
     case ADD_POST:
       return { ...state, posts: [...state.posts, action.payload] };
     case DELETE_POST:
-      return { ...state, }
+      return { ...state, posts: state.posts.filter(({ postId }) => postId !== action.payload) };
   }
 }
  
