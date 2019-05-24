00d6c633e4ec61161b504586746100b90a9697fe
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-05-24 15:38:37 -0700

    adding comments to the post page

--- src/containers/PostById.js
+++ src/containers/PostById.js
@@ -1,12 +1,22 @@
 import { connect } from 'react-redux';
 import PostDetail from '../components/PostDetail';
 import { getPost } from '../selectors/postSelectors';
+import { getComments } from '../selectors/commentSelectors';
+import { addComment } from '../actions/commentActions';
 
 const mapStateToProps = (state, props) => ({
-  post: getPost(state, props.match.params.id)
+  post: getPost(state, props.match.params.id),
+  comments: getComments(state, props.match.params.id)
+});
+
+const mapDispatchToProps = dispatch => ({
+  onSubmit(comment) {
+    dispatch(addComment(comment));
+  }
 });
 
 
 export default connect(
-  mapStateToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(PostDetail);
